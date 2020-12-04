a518fb519c6340a455db5628ffb4086b1c48538f
Michael Xuan <michaelxuan@hotmail.com>
2020-12-04 11:47:05 +0800

    blurprint is done

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 * This demo has been following the freecodecamp flask tutorial step by step.
 * Back bone of future study and personal blog.
 
+# Code notes
+* What if the browser won't refresh and update  
+  https://www.cnblogs.com/zhenggaoxiong/p/9465440.html
+
+* Connect to database with flask_sqlalchemy
+* https://www.iditect.com/how-to/53508591.html
+  https://hackersandslackers.com/flask-sqlalchemy-database-models
+  https://docs.sqlalchemy.org/en/14/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc
+
+
+
+
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Import flask class, html render and the url decorator
-from flask import Flask, render_template, url_for   
+from flask import Flask, render_template, url_for, request, redirect
 # Flask database configuration
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+
 
 
 # Base class of flask where it will starts the web service
@@ -12,19 +14,57 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'     # 3 ///s with re
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'  # 4 ////s with absolute path 
 db = SQLAlchemy(app)
 
+# set up timer for cache clean
+# https://www.cnblogs.com/zhenggaoxiong/p/9465440.html
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+# app.send_file_max_age_default = timedelta(seconds=1)
+
 
 class Todo(db.Model):
-    
+    """ 
+    - Inherited from SQLAlchemy Base class 
+    - Base class for SQLAlchemy declarative base model.
+    - from flask_sqlalchemy.model import Model  flask_sqlalchemy.__init__.py
+    - class SQLAlchemy(object):
+    - ...
+    -  This class also provides access to all the SQLAlchemy functions and classes
+    from the :mod:`sqlalchemy` and :mod:`sqlalchemy.orm` modules.  So you can
+    declare models like this::
+
+        class User(db.Model):
+            username = db.Column(db.String(80), unique=True)
+            pw_hash = db.Column(db.String(80))
+    """  
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task   %r>' % self.id
+
 
 
 
 
 # Url configurations
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 
 def index():
-    # return 'hELLO worlD!!!'
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['post_content_name']
+        new_task = Todo(content=task_content)
+
+        try:
+            # add data to the db through db session
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')   # redirect the page back to the root path
+        except:
+            return 'Error occurred while adding the new task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 # Main
